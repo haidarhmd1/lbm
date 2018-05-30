6c3c4e54a7309091b6943160fcfb7bdac2d6ff4a
haidarhmd1 <haidar.hmd1@gmail.com>
2018-05-30 15:18:56 +0300

    restablished

--- config/init/init.php
+++ config/init/init.php
@@ -12,14 +12,14 @@
 		private $dbname;
 
 		public function connect(){
-
-			$this->servername = "localhost";
+			//69.73.184.9
+			$this->servername = "69.73.182.184";
 			$this->username = "lebmotor_motors2";
 			$this->password = "admin@123";
 			$this->dbname = "lebmotor_lebmotor";
 
 			$conn = new mysqli($this->servername,$this->username,$this->password,$this->dbname);
-			
+
 			if($conn->connect_error){
 				echo "error";
 			}else{
